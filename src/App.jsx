26d854bd7229d250a0a327c07b11824eab2a8aaa
solenecrzt01 <solenecrzt01@gmail.com>
2026-09-08 01:48:50 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1310,10 +1310,10 @@ body {
 }
 .image-pensee {
   object-fit: cover;
-  object-position: 56% 42% !important;
+  object-position: 60% 34% !important;
 }
 .featured-tile:hover .featured-art img.image-pensee {
-  object-position: 56% 42% !important;
+  object-position: 60% 34% !important;
 }
 
 .featured-meta {
