25c1a5580a0b0732c37f9603651b1c4594e98b59
solenecrzt01 <solenecrzt01@gmail.com>
2026-09-07 18:20:00 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1286,7 +1286,7 @@ body {
   overflow: hidden;
 }
 .image-binche {
-  transform: scale(1.2) translateY(18px);
+  transform: scale(1.2) translateY(16px);
 }
   
 .featured-tile:hover .featured-art svg { transform: scale(1.06); }
